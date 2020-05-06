38c08608a58c6a88e14d226d8775487dfc9faa9c
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-06 19:55:10 -0400

    printString method working

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0]);
+
+  if (string.length > 1) {
+    let mySubString = string.substring(1, string.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}

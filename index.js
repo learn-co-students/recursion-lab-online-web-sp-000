d9304ef93bd8503b1b1fcd055039261f66361e83
deliaconstantino <dconstan92@gmail.com>
2021-05-13 23:48:53 -0400

    Done.

--- index.js
+++ index.js
@@ -10,26 +10,35 @@ function printString(str) {
 
   if (substr.length > 0) {
     printString(substr);
-  } else {
-    return true;
   }
+  // else {
+  //   return true;
+  // }
 }
 
-function reverseString(str, reversedArray = []) {
-  if (reversedArray) {
-    reversedArray.push(str[str.length - 1]);
-  } else {
-    reversedArray = [str[str.length - 1]];
-  }
+// function reverseString(str, reversedArray = []) {
+//   if (reversedArray) {
+//     reversedArray.push(str[str.length - 1]);
+//   }
+//   // else {
+//   //   reversedArray = [str[str.length - 1]];
+//   // }
 
-  if (str.length > 1) {
-    let newString = str.substring(0, str.length - 1);
-    reverseString(newString, reversedArray);
-  } else {
-    return reversedArray;
-  }
+//   if (str.length > 1) {
+//     let newString = str.substring(0, str.length - 1);
+//     reverseString(newString, reversedArray);
+//   }
 
-  return reversedArray.join("");
+//   return reversedArray.join("");
+// }
+
+function reverseString(str, reversedString = "") {
+  if (str.length === 0) return reversedString;
+
+  const newString = str.substring(0, str.length - 1);
+  const newReversedString = reversedString.concat(str[str.length - 1]);
+
+  return reverseString(newString, newReversedString);
 }
 
 function isPalindrome(str, i = 0, j = str.length - 1) {
@@ -45,43 +54,43 @@ function isPalindrome(str, i = 0, j = str.length - 1) {
 
   return answer;
 }
-
-function addUpTo(array, idx, i = 0, sum = 0) {
-  sum = sum + array[i]
-  if (i < idx) {
-    sum = addUpTo(array, idx, i + 1, sum)
-  } else {
-    return sum
-  }
-
-  return sum
-} //passing but how does it work?
 //looking for one case that makes it false, default is true
 
+function addUpTo(array, idx, i = 0, sum = 0) {
+  sum = sum + array[i];
+
+  if (i < idx) {
+    sum = addUpTo(array, idx, i + 1, sum);
+  }
+
+  return sum;
+
+  // const newSum = sum + array[i];
+  // if (i >= idx) return newSum;
+  // return addUpTo(array, idx, i + 1, newSum);
+}
 
 // if (i = 0) {
-  //   highestSeen = array[i]
-  // } //better to do this commented out bit or pass in to definition?
+//   highestSeen = array[i]
+// } //better to do this commented out bit or pass in to definition?
 function maxOf(array, i = 0, highestSeen = array[i]) {
-
   if (array[i] > highestSeen) {
-    highestSeen = array[i]
+    highestSeen = array[i];
   }
 
   if (i < array.length - 1) {
-    highestSeen = maxOf(array, i + 1, highestSeen)
-  } else {
-    return highestSeen
+    highestSeen = maxOf(array, i + 1, highestSeen);
   }
+  // else {
+  //   return highestSeen
+  // }
 
-  return highestSeen
-
+  return highestSeen;
 }
 //           i
 // [1, 4, 5, 3]
 //           3
 // 4: array[i] = 3; i = 3; highestSeen = 5
-
 
 // call 4 = return 5
 // call 3 = return of call 4, so return 5
@@ -91,17 +100,18 @@ function maxOf(array, i = 0, highestSeen = array[i]) {
 function includesNumber(array, target, i = 0) {
   let answer = false;
   if (i < array.length) {
-    if(array[i] !== target) {
-      answer = includesNumber(array, target, i + 1)
+    if (array[i] !== target) {
+      answer = includesNumber(array, target, i + 1);
     } else {
       answer = true;
       return answer;
     }
   } else {
-    return answer
+    return answer;
   }
   return answer;
 }
+//looking for the one case that makes it true, default is false
 
 // [1, 4, 2, 3] target = 5
 //             i
@@ -116,13 +126,6 @@ function includesNumber(array, target, i = 0) {
 // answer round 3 = return of round 4
 // answer round 2 = return of round 3
 // answer round 1 = return of round 2
-
-
-// what am i doing wrong above? i have it going for 5 rounds but it passes the test that says just 4
-
-
-
-
 // ------
 // [1, 4, 5, 3]   target = 5;
 //        i
@@ -132,14 +135,6 @@ function includesNumber(array, target, i = 0) {
 // array.length = 4;
 // round 3
 
-
 // round 3 = true
 // round 2 = return of round 3 = true
 // round 1 = return of round 2 = true
-
-
-
-
-
-
-//looking for the one case that makes it true, default is false
